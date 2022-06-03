9e3fe88741f0c967fcabab539669f13000e40194
HristakaZ <hristaka2013@abv.bg>
2022-06-03 17:28:38 +0300

    Added change of each letter's color of the welcome heading

--- Angular-CRUD-Project/src/app/welcome/welcome.component.ts
+++ Angular-CRUD-Project/src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'welcome-component',
@@ -6,7 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 
-export class WelcomeComponent {
+export class WelcomeComponent implements AfterViewInit {
   readonly gifPlaybackLoopUrl: string = 'assets/backgroundLoop.gif';
   readonly welcomeHeading: string = "Welcome!";
+  @ViewChild('welcomeHeadingId') welcomeHeadingElement!: ElementRef<HTMLHeadingElement>;
+  randomRGBColor: string = '';
+
+  getRandomRGBColor(): string {
+    let rgb: number[] = [];
+
+    for(let i = 0; i < 3; i++) {
+        rgb.push(Math.floor(Math.random() * 255));
+    }
+
+    let combinedRgbColors: string = 'rgb('+ rgb.join(',') +')';
+
+    return combinedRgbColors;
+  }
+
+  ngAfterViewInit(): void {
+    const source = interval(1000);
+    source.subscribe(() => {
+      this.welcomeHeadingElement.nativeElement.innerHTML = '';
+      for(let char: number = 0; char < this.welcomeHeading.length; char++) {
+        this.randomRGBColor = this.getRandomRGBColor();
+        let span: string = `<span style="color:${this.randomRGBColor}">${this.welcomeHeading[char]}</span>`;
+        this.welcomeHeadingElement.nativeElement.style.color = this.randomRGBColor;
+        this.welcomeHeadingElement.nativeElement.innerHTML += span;
+      }
+    });
+  }
 }
