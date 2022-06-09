d4c050f560f92e650bda1d10327f5b4da930b7ca
HristakaZ <hristaka2013@abv.bg>
2022-06-09 15:17:05 +0300

    Added relationship between game studio and games

--- Angular-CRUD-Project/src/app/game-studio/read/game-studio.component.ts
+++ Angular-CRUD-Project/src/app/game-studio/read/game-studio.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { GameStudio } from '../game-studio.model';
 import { GameStudioService } from '../services/game-studio.service';
+import { GameService } from '../services/game.service';
 
 @Component({
   selector: 'game-studio-component',
@@ -11,19 +12,29 @@ import { GameStudioService } from '../services/game-studio.service';
 export class GameStudioComponent implements OnInit, AfterViewInit {
     public gameStudios!: GameStudio[];
     public columnsToDisplay = ['name', 'dateOfEstablishment', 'mainOffice', 'games', 'updateButton', 'deleteButton'];
-    constructor(private gameStudioService: GameStudioService) {
+    constructor(private gameStudioService: GameStudioService, private gameService: GameService) {
 
     }
 
     ngOnInit(): void {
       this.gameStudioService.getAll().subscribe((gameStudios) => {
         this.gameStudios = gameStudios;
+        this.gameStudios.forEach(gameStudio => {
+          this.gameService.getAll().subscribe(games => {
+            gameStudio.games = games.filter(game => game.gameStudio.id === gameStudio.id);
+          });
+        });
       });
     }
     
     ngAfterViewInit(): void {
       this.gameStudioService.getAll().subscribe((gameStudios) => {
         this.gameStudios = gameStudios;
+        this.gameStudios.forEach(gameStudio => {
+          this.gameService.getAll().subscribe(games => {
+            gameStudio.games = games.filter(game => game.gameStudio.id === gameStudio.id);
+          });
+        });
       });
     }
 }
